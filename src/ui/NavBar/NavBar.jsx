fdd152d4010417d456727f3ad0db068479a10928
Brian <sistemashoss@gmail.com>
2025-09-17 14:54:51 -0600

    correcion simple

--- src/ui/NavBar/NavBar.jsx
+++ src/ui/NavBar/NavBar.jsx
@@ -93,11 +93,11 @@ export default function Navbar() {
                                         Perfil
                                     </MDBDropdownToggle>
                                     <MDBDropdownMenu>
-                                        <MDBDropdownItem link>
+                                        <MDBDropdownItem>
                                             {/* Muestra email de Auth0 si disponible, sino de Redux */}
                                             {auth0User?.email || user?.userEmail || "Usuario"}
                                         </MDBDropdownItem>
-                                        <MDBDropdownItem link>Información Personal</MDBDropdownItem>
+                                        <MDBDropdownItem >Información Personal</MDBDropdownItem>
                                         {rol === "admin" && (
                                             <>
                                             <MDBDropdownItem link href="/compras">
