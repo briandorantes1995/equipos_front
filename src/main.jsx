93a7e0a6fc4bef2f65ea7518c3b6e90fafc1cc77
Brian <sistemashoss@gmail.com>
2025-09-19 17:00:49 -0600

    navbar funciona en movil, cambios casi finales de css funcional

--- src/main.jsx
+++ src/main.jsx
@@ -9,7 +9,6 @@ import App from './App.jsx'
 import 'mdb-react-ui-kit/dist/css/mdb.min.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import "@fortawesome/fontawesome-free/css/all.min.css";
-import './index.css'
 
 
 createRoot(document.getElementById('root')).render(
