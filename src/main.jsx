78ebb0d8493929269f4264cdb4c0d792a9351560
Brian <sistemashoss@gmail.com>
2025-09-19 17:02:28 -0600

    navbar funciona en movil, cambios casi finales de css funcional

--- src/main.jsx
+++ src/main.jsx
@@ -5,10 +5,11 @@ import {PersistGate} from "redux-persist/integration/react";
 import {Provider} from "react-redux";
 import { store, persistor } from './store/store.jsx';
 import {SnackbarProvider} from "./ui/snackBar/SnackBarProvider.jsx";
-import App from './App.jsx'
 import 'mdb-react-ui-kit/dist/css/mdb.min.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import "@fortawesome/fontawesome-free/css/all.min.css";
+import './index.css'
+import App from './App.jsx'
 
 
 createRoot(document.getElementById('root')).render(
