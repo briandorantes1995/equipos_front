2df79bff98b19844ab67152e6beb31920d1b5d36
Brian <sistemashoss@gmail.com>
2025-09-19 15:44:14 -0600

    css

--- src/Functions/useMobile.js
+++ src/Functions/useMobile.js
@@ -1,6 +1,6 @@
 import { useState, useEffect } from "react";
 
-export function useMobile(maxWidth = 576) {
+export default function useMobile(maxWidth = 576) {
   const [isMobile, setIsMobile] = useState(window.innerWidth <= maxWidth);
 
   useEffect(() => {
