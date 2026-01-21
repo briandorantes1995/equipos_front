1a8fced2edcbea7c1577d53e2bc7ee0f35f17932
briandorantes <briandorantes@hotmail.com>
2026-01-20 22:47:55 -0600

    catologo de ventas

--- src/Functions/obtenerCatalogoPDF.js
+++ src/Functions/obtenerCatalogoPDF.js
@@ -2,10 +2,18 @@ import axios from "axios";
 
 export async function obtenerCatalogoPDF() {
   const url = `${import.meta.env.VITE_BACKEND_URL}/api/articulos/catalogo-pdf`;
-
+  try{
   const response = await axios.get(url, {
     responseType: "blob", // 🔑 CLAVE
   });
+  }catch (error) {
+  if (error.response?.data instanceof Blob) {
+    const text = await error.response.data.text();
+    console.error("PDF error real:", text);
+  } else {
+    console.error(error);
+  }
+}
 
   return response.data; // Blob PDF
 }
